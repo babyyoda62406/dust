497f9528e95f0a5b7036930f95faca6a12340f37
David Lezcano Becerra <davidbeceralezcano@gmail.com>
2023-08-08 00:34:35 -0400

    fix(dlb/front): arreglado stars

--- front/src/Utils/Utils.tsx
+++ front/src/Utils/Utils.tsx
@@ -1,10 +1,28 @@
  
 
-export const drawStars = (stars: number) => {
+export const drawStars = (stars: number) => { 
+  const n: number = stars / 2
+  let a: number[] = []
 
-    return (
-      <div className="d-flex">
-        <span className="bi bi-star-fill text-warning"></span> 
-      </div>
-    )
+ 
+  for (let i = 1; i < 6; i++) {
+    if (i <= n) {
+      a.push(1) 
+    }
+    else {
+      a.push(0) 
+    }
+  }
+  return (
+    <div className="d-flex">
+      { a.map((i) => {
+        if (i == 1) { 
+          return (<span className="bi bi-star-fill text-warning"></span>)
+        }
+        else { 
+          return (<span className="bi bi-star text-warning"></span>)
+        }
+      }) }
+    </div>
+  )
 }
